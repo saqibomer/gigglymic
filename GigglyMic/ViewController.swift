c4ee94c4118a67fe71dad630c174cb1bf985d742
Saqib Omer <TOxIC@macbookairs-MacBook-Air.local>
2015-09-02 13:49:41 +0500

    Initial Commit

---
+++ GigglyMic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GigglyMic
+//
+//  Created by Saqib Omer on 9/2/15.
+//  Copyright (c) 2015 Kaboom Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
